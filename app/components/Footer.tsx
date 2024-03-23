2787e820f8a9a315999f5fe4fff75ceacdfd3547
Abdifatah Bashi <abdifatah.bashi@bekk.no>
2024-03-23 13:58:34 +0100

    Legger til responsive footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,10 +3,10 @@ import Image from 'next/image';
 export function Footer() {
   return (
     <footer>
-      <div className="container mx-auto py-8">
-        <div className="flex flex-col md:flex-row justify-between items-center">
-          <div className="mb-4 md:mb-0">
-            <a href="#" className="flex items-center">
+      <div className="bg-background px-8 py-4 ">
+        <div className="flex flex-col md:flex-row ">
+          <div className="mb-4 md:mb-0 ">
+            <a href="#">
               <Image
                 src="/friggmedia/logo.svg"
                 alt="Friggmedia Logo"
@@ -18,8 +18,8 @@ export function Footer() {
           </div>
         </div>
 
-        <div className="flex flex-col  md:flex-row justify-between items-center mt-8">
-          <div className="flex flex-wrap gap-6 justify-center">
+        <div className="flex flex-col  md:flex-row md:justify-between md:items-center   ">
+          <div className="flex gap-6 ">
             <a href="#" className="text-gray-700 text-lg hover:text-blue-600">
               Produkter
             </a>
@@ -30,9 +30,11 @@ export function Footer() {
               Kontakt oss
             </a>
           </div>
-          <div className="flex gap-6 items-center ">
-            <span className="text-gray-700 text-lg mb-4 md:mb-0">&copy; Frigg Media AS 2024</span>
-            <div className="flex space-x-4">
+          <div className="flex gap-6">
+            <span className="text-gray-700 items-center text-lg my-4 ">
+              &copy; Frigg Media AS 2024
+            </span>
+            <div className="flex space-x-4 items-center">
               <a href="#" aria-label="Facebook" className="text-blue-800 hover:text-blue-600">
                 <Image
                   src="/friggmedia/facebook.svg"
