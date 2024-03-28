dca12eaf25732fe8f6a63378a62d2f495a5096bd
Abdifatah Bashi <abdifatah.bashi@bekk.no>
2024-03-28 13:53:44 +0100

    Oppdater navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,55 +1,67 @@
 'use client';
 
-import React, { useState } from 'react';
+import React from 'react';
+import {
+  Navbar as NUINavbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+  Link,
+} from '@nextui-org/react';
 import { Logo } from './Logo';
-import ThemeSwitch from './ThemeSwitch';
 
-export const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
+export function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const menuItems = ['Om oss', 'Kontakt oss'];
 
   return (
-    <header className="sm:flex sm:justify-between sm:items-center sm:px-4 sm:py-3">
-      <div className="flex items-center justify-between px-4 py-3 sm:p-0">
-        <div>
+    <NUINavbar
+      maxWidth="full"
+      className="px-8 justify-between"
+      classNames={{ wrapper: 'px-0' }}
+      onMenuOpenChange={setIsMenuOpen}
+      isBlurred={false}
+    >
+      <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
+          className="sm:hidden"
+        />
+        <NavbarBrand>
           <Logo />
-        </div>
-        <div className="sm:hidden">
-          <button
-            onClick={() => setIsOpen(!isOpen)}
-            type="button"
-            className="block text-gray-500 hover:text-white focus:text-white focus:outline-none"
-          >
-            <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
-              {isOpen ? (
-                <path
-                  fillRule="evenodd"
-                  d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
-                />
-              ) : (
-                <path
-                  fillRule="evenodd"
-                  d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
-                />
-              )}
-            </svg>
-          </button>
-        </div>
-      </div>
-      <nav
-        className={`${isOpen ? 'block' : 'hidden'} px-2 pt-2 pb-4 sm:flex sm:p-0 flex items-center`}
-      >
-        <div className="flex gap-6 items-center ">
-          <a href="#" className="light:opacity-55 text-xl hover:text-blue-600">
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent justify="end" className="hidden sm:flex">
+        <NavbarItem>
+          <Link color="foreground" className="opacity-55 text-xl hover:text-blue-600" href="#">
             Om oss
-          </a>
-          <a href="#" className="light:opacity-55 text-xl hover:text-blue-600">
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link
+            color="foreground"
+            className="opacity-55 text-xl hover:text-blue-600"
+            href="#"
+            aria-current="page"
+          >
             Kontakt oss
-          </a>
-          <a href="#" className="light:opacity-55 text-xl hover:text-blue-600">
-            <ThemeSwitch />
-          </a>
-        </div>
-      </nav>
-    </header>
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link color="foreground" className="w-full" href="#" size="lg">
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </NUINavbar>
   );
-};
+}
--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,12 @@ import { Hero } from './components/Hero';
 import { CallToAction } from './components/CallToAction';
 import { Products } from './components/Products';
 import { Navbar } from './components/Navbar';
-import { Nav } from './components/Nav';
 
 export default function Home() {
   return (
     <div className="flex flex-col justify-between h-lvh">
-      <Nav />
-      {/* <Navbar /> */}
-
+      <Navbar />
       <Hero />
-      <div className="bg-background h-full"></div>
       <Products />
       <CallToAction />
       <Footer />
