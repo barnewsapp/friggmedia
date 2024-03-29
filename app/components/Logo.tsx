f6f13146f1c95ec2a4a14a2b75e29e1cc49b0c36
Abdifatah Bashi <abdifatah.bashi@bekk.no>
2024-03-29 12:28:33 +0100

    Legger Logo placeholder

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,10 +1,41 @@
 import Image from 'next/image';
 import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
+import { Skeleton } from '@nextui-org/react';
+
+export default function LogoPlaceholder() {
+  return (
+    <div className="max-w-[300px] w-full flex items-center gap-3">
+      <div>
+        <Skeleton className="flex rounded-full w-12 h-12" />
+      </div>
+      <div className="w-full flex flex-col gap-2">
+        <Skeleton className="h-4 w-3/5 rounded-lg" />
+      </div>
+    </div>
+  );
+}
 
 export function Logo() {
+  const [isMounted, setIsMounted] = useState(false);
+
   const { resolvedTheme } = useTheme();
 
-  const logo = resolvedTheme === 'dark' ? './logo-dark.svg' : './logo.svg';
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
-  return <Image src={logo} alt="Friggmedia Logo" width={175} height={40} priority />;
+  if (!isMounted) {
+    return <LogoPlaceholder />;
+  }
+
+  return (
+    <Image
+      src={resolvedTheme === 'dark' ? '/logo-dark.svg' : '/logo.svg'}
+      alt="Friggmedia Logo"
+      width={175}
+      height={40}
+      priority
+    />
+  );
 }
